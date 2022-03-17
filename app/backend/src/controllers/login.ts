f4cc46a0c3ade02b83993a8f56d1a6ca2e85e92a
Dogl4 <doougllas@hotmail.com.br>
2022-03-17 17:14:06 -0300

    Update files: `login.ts`:(controllers), `login.ts`:(services). Verify inputs and validate data with the database

--- app/backend/src/controllers/login.ts
+++ app/backend/src/controllers/login.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { Request, Response, NextFunction, Router } from 'express';
 import Login from '../services';
 import { ILogin } from '../interfaces/login';
 
@@ -10,15 +10,31 @@ class LoginController {
     this.routes();
   }
 
+  public static async verifyData(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body as ILogin;
+    if (!email || !password) {
+      res.status(401).json({ message: 'All fields must be filled' }).end();
+    }
+
+    const error = await Login.verifyPassword({ email, password });
+    if (error || !/\S+@\S+\.\S+/.test(email)) {
+      res.status(401).json({ message: 'Incorrect email or password' }).end();
+    }
+
+    next();
+  }
+
   public static async logIn(req: Request, res: Response) {
-    const redBody: ILogin = req.body;
-    /* const result =  */await Login.verifiLogin(redBody);
+    const { email } = req.body as ILogin;
+
+    const user = await Login.getUser({ email });
+    console.log('user ==>', user);
 
     res.status(200).json({ message: 'Hello, login' });
   }
 
   routes() {
-    this.router.post('/', LoginController.logIn);
+    this.router.post('/', LoginController.verifyData, LoginController.logIn);
   }
 }
 
--- app/backend/src/services/login.ts
+++ app/backend/src/services/login.ts
@@ -1,9 +1,26 @@
+import * as bcrypt from 'bcryptjs';
 import type { ILogin } from '../interfaces/login';
+import User from '../database/models/user';
 
 class Login {
-  static verifiLogin(reqRoby: ILogin) {
-    // const { email, password } = reqRoby;
-    console.log('Deu bom aaaaaa', reqRoby);
+  static async verifyPassword({ email, password }: ILogin) {
+    const user = await User.findOne({ where: { email }, raw: true });
+    if (!user) {
+      return true;
+    }
+
+    const passwordDB: string = user.password;
+    const testPassword = await bcrypt.compare(password, passwordDB);
+    if (!testPassword) {
+      return true;
+    }
+  }
+
+  static async getUser({ email }: { email: string; }) {
+    const user = await User.findOne({
+      where: { email }, raw: true, attributes: { exclude: ['password'] },
+    });
+    return user;
   }
 }
 
